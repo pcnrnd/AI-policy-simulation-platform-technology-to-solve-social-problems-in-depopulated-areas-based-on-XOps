af527aa830553741c27a275b79e2a748e14b6593
jwlee <jwlee@pcninc.co.kr>
2024-10-24 17:51:44 +0900

    create chart speed and multiselect

---
+++ chart_speed/lib/exception_handling.py
@@ -0,0 +1,11 @@
+import time
+import random
+
+class ExceptionHandling:
+    def __init__(self) -> None:
+        pass
+
+    def exception_check(self):
+        value = random.uniform(0.58, 0.63)
+        time.sleep(value)
+
