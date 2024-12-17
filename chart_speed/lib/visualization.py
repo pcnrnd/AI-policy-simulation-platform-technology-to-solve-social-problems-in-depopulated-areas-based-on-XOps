ebbd074f3be8114676acc0329c6252346e8b1695
jwlee <jwlee@pcninc.co.kr>
2024-12-17 15:50:28 +0900

    refactor visuaization.py

--- chart_speed/lib/visualization.py
+++ chart_speed/lib/visualization.py
@@ -8,6 +8,10 @@ class Visualization:
         pass
 
     def bar_chart(self, df, group):
+        '''
+        df: dataframe \n
+        group: column name
+        '''
         selected_df = pd.DataFrame(df.groupby(group)['num'].agg('sum')).reset_index()
         fig = px.bar(selected_df, x=group, y='num', color=group)
         fig.update_layout(
@@ -16,11 +20,21 @@ class Visualization:
         st.plotly_chart(fig)
 
     def funnel_chart(self, df, group_a, group_b):
+        '''
+        df: dataframe \n
+        group_a: column name a
+        group_b: column name b
+        '''
         selected_df = pd.DataFrame(df.groupby([group_a, group_b])['num'].agg('sum')).reset_index()
         fig = px.funnel(selected_df, x='num', y=group_a, color=group_b)
         st.plotly_chart(fig)
 
     def scatter_chart(slef, df, group_a, group_b):
+        '''
+        df: dataframe \n
+        group_a: column name a \n
+        group_b: column name b
+        '''
         category_df = df.groupby([group_a, group_b])['num'].agg('sum').reset_index()
         fig = px.scatter(category_df, x=group_a, y='num', color=group_b)
         fig.update_layout(
