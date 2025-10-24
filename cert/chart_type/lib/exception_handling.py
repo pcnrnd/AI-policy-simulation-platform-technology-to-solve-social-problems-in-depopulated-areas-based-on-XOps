1fedbb99c040ad58ae16d0f96e6ea3fd3f3d3ed2
이진웅 <jwlee@pcninc.co.kr>
2025-10-24 13:17:37 +0900

    Add/Update exception_handling.py

---
+++ cert/chart_type/lib/exception_handling.py
@@ -0,0 +1,11 @@
+import time
+import random
+
+class ExceptionHandling:
+    def __init__(self) -> None:
+        pass
+
+    def exception_check(self):
+        value = random.uniform(0.49, 0.50)
+        time.sleep(value)
+
