ee6ed63ccd75fab8246eeb308e3e96b513dc525d
이진웅 <jwlee@pcninc.co.kr>
2026-06-10 14:35:00 +0900

    Add/Update main.jsx

--- decline_poc/src/main.jsx
+++ decline_poc/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AppStateProvider } from "./context/AppStateContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import "./lib/chartSetup.js";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppStateProvider>
-      <App />
-    </AppStateProvider>
+    <ThemeProvider>
+      <AppStateProvider>
+        <App />
+      </AppStateProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
